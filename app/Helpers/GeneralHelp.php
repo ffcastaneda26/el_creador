9e88a15284d45843a2c7a7f2d75b65d9be683fa2
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2024-11-06 12:38:01 -0700

    Se eliminan comentarios y espacios en clase GeneralHelper

--- app/Helpers/GeneralHelp.php
+++ app/Helpers/GeneralHelp.php
@@ -75,7 +75,6 @@ class GeneralHelp
 
 
     }
-
     /**
      * Regresa el número convertido a letras
      * @return string
@@ -87,8 +86,6 @@ class GeneralHelp
         $converter = new NumerosALetras();
         $converter->anexar = null;
         return ucwords(strtolower($converter->toInvoice($number,$decimals,$text))) . 'M/N';
-
     }
-
-
+    
 }
