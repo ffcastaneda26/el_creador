1e5fb8177cbeb3c336ba23125fd0b22e547af4b1
Federico Castañeda <ffcastaneda@gmail.com>
2025-03-05 14:14:06 -0600

    Ajustes generales a CRUD de códigos postales

--- app/Filament/Resources/ZipcodeResource.php
+++ app/Filament/Resources/ZipcodeResource.php
@@ -4,23 +4,24 @@ namespace App\Filament\Resources;
 
 use Filament\Forms;
 use Filament\Tables;
-use App\Models\State;
-use App\Models\Country;
 use App\Models\Zipcode;
 use Filament\Forms\Form;
 use Filament\Tables\Table;
-use App\Models\Municipality;
 use Filament\Resources\Resource;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
 use Filament\Tables\Filters\SelectFilter;
 use Illuminate\Database\Eloquent\Builder;
 use App\Filament\Resources\ZipcodeResource\Pages;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
-use App\Filament\Resources\ZipcodeResource\RelationManagers;
+use App\Models\Country;
+use App\Models\Municipality;
+use App\Models\State;
+use Filament\Forms\Components\Group;
 
 class ZipcodeResource extends Resource
 {
     protected static ?string $model = Zipcode::class;
-
 
     protected static ?string $navigationIcon = 'heroicon-o-map-pin';
     protected static ?string $activeNavigationIcon = 'heroicon-s-shield-check';
@@ -46,60 +47,55 @@ class ZipcodeResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\Group::make()->schema([
-                    Forms\Components\TextInput::make('zipcode')
+                Group::make()->schema([
+                    TextInput::make('zipcode')
                         ->required()
                         ->numeric()
-                        ->length(5)
-                        ->translateLabel()
-                        ->validationMessages([
-                            'length' => __('You must enter five digits'),
-                        ]),
-                    Forms\Components\TextInput::make('name')
+                        ->maxLength(5)
+                        ->minLength(5),
+                        TextInput::make('name')
                         ->required()
                         ->maxLength(100)
-                        ->minLength(5)
-                        ->label(__('Colony'))
                         ->columnSpan(2),
-                    Forms\Components\Select::make('type_zipcode_id')
-                        ->relationship(
+                    Select::make('type_zipcode_id')
+                    ->relationship(
                             name: 'type',
                             titleAttribute: 'type'
                         )
-                        ->required()
-                        ->preload()
-                        ->translateLabel()
-                        ->columnSpan(2)
+                    ->required()
+                    ->preload()
+                    ->translateLabel()
+                    ->columnSpan(2)
 
                 ])->columns(3),
-                Forms\Components\Group::make()->schema([
-                    Forms\Components\Select::make('country_id')
-                        ->relationship(
+                Group::make()->schema([
+                    Select::make('country_id')
+                    ->relationship(
                             name: 'country',
                             titleAttribute: 'country',
-                            modifyQueryUsing: fn(Builder $query) => $query->where('include', 1),
+                            modifyQueryUsing: fn (Builder $query) => $query->where('include',1),
                         )
-                        ->required()
-                        ->reactive()
-                        ->preload()
-                        ->default(135)
-                        ->searchable(['country', 'code'])
-                        ->translateLabel()
-                        ->afterStateUpdated(fn(callable $set) => $set('state_id', null)),
+                    ->required()
+                    ->reactive()
+                    ->preload()
+                    ->default(135)
+                    ->searchable(['country', 'code'])
+                    ->translateLabel()
+                    ->afterStateUpdated(fn (callable $set) => $set('state_id', null)),
 
-                    Forms\Components\Select::make('state_id')
-                        ->translateLabel()
-                        ->required()
-                        ->reactive()
-                        ->options(function (callable $get) {
-                            $country = Country::find($get('country_id'));
-                            if (!$country) {
-                                return;
-                            }
-                            return $country->states->pluck('name', 'id');
-                        })->afterStateUpdated(fn(callable $set) => $set('municipality_id', null)),
+                Select::make('state_id')
+                    ->translateLabel()
+                    ->required()
+                    ->reactive()
+                    ->options(function (callable $get) {
+                        $country = Country::find($get('country_id'));
+                        if (!$country) {
+                            return;
+                        }
+                        return $country->states->pluck('name', 'id');
+                    })->afterStateUpdated(fn (callable $set) => $set('municipality_id', null)),
 
-                    Forms\Components\Select::make('municipality_id')
+                Select::make('municipality_id')
                         ->translateLabel()
                         ->required()
                         ->reactive()
@@ -109,19 +105,19 @@ class ZipcodeResource extends Resource
                                 return;
                             }
                             return $state->municipalities->pluck('name', 'id');
-                        })->afterStateUpdated(fn(callable $set) => $set('city_id', null)),
+                    })->afterStateUpdated(fn (callable $set) => $set('city_id', null)),
 
-                    Forms\Components\Select::make('city_id')
-                        ->translateLabel()
-                        ->required()
-                        ->reactive()
-                        ->options(function (callable $get) {
-                            $municipality = Municipality::find($get('municipality_id'));
-                            if (!$municipality) {
-                                return;
-                            }
-                            return $municipality->cities->pluck('name', 'id');
-                        }),
+                Select::make('city_id')
+                    ->translateLabel()
+                    ->required()
+                    ->reactive()
+                    ->options(function (callable $get) {
+                        $municipality = Municipality::find($get('municipality_id'));
+                        if (!$municipality) {
+                            return;
+                        }
+                        return $municipality->cities->pluck('name', 'id');
+                    }),
                 ])->columns(2),
             ]);
     }
@@ -130,27 +126,30 @@ class ZipcodeResource extends Resource
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('zipcode')
+                TextColumn::make('country.country')
+                    ->searchable()
+                    ->translateLabel()
+                    ->sortable(),
+                TextColumn::make('state.name')
+                    ->searchable()
+                    ->translateLabel()
+                    ->sortable(),
+                TextColumn::make('municipality.name')
+                    ->searchable()
+                    ->translateLabel()
+                    ->sortable(),
+                TextColumn::make('city.name')
+                    ->searchable()
+                    ->translateLabel()
+                    ->sortable(),
+                TextColumn::make('zipcode')
                     ->label(__('Zipcode'))
                     ->searchable(),
-                Tables\Columns\TextColumn::make('state.name')
-                    ->searchable()
-                    ->translateLabel()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('municipality.name')
-                    ->searchable()
-                    ->translateLabel()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('city.name')
-                    ->searchable()
-                    ->translateLabel()
-                    ->sortable(),
-
-                Tables\Columns\TextColumn::make('name')
+                TextColumn::make('name')
                     ->translateLabel()
                     ->sortable()
                     ->searchable(),
-                Tables\Columns\TextColumn::make('type.type')
+                TextColumn::make('type.type')
                     ->searchable()
                     ->translateLabel(),
             ])
