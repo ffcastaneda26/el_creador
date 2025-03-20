e8ea625869a79a9932ac5ea0cbc33cae61def137
Federico Castañeda <ffcastaneda@gmail.com>
2025-03-20 15:38:31 -0600

    Surtido de Solicitudes de Material: Se corrige error al surtir (Es necesario que en tabla key_movements esté un registro con short="SolAlm"

--- app/Livewire/WarehouseRequests/WareHouseRequestDetail.php
+++ app/Livewire/WarehouseRequests/WareHouseRequestDetail.php
@@ -39,14 +39,15 @@ class WareHouseRequestDetail extends Component
      *
      */
     public function updateItem()
-    {   
-
+    {
          if($this->ware_request_detail->getPending() == 0 || !$this->validate_quantity()   ){
             return;
         }
 
+
         try {
             $this->ware_request_detail->updateDelivery($this->quantity);
+            $key_movement = KeyMovement::where('short','SolAlm')->first();
             $movement = $this->create_movement();
             $this->ware_request_detail->warehouse_request->updateStatus();
             $this->ware_request_detail->refresh();
@@ -68,6 +69,7 @@ class WareHouseRequestDetail extends Component
       */
      public function create_movement()
      {
+
         $key_movement = KeyMovement::where('short','SolAlm')->first();
         $movement = Movement::create([
             'warehouse_id' => $this->product_warehouse->warehouse->id,
@@ -87,5 +89,19 @@ class WareHouseRequestDetail extends Component
      public function update_stock(Movement $movement){
         $this->product_warehouse->updateStock($movement->quantity,$movement->key_movement->type);
         $this->product_warehouse->refresh();
+     }
+
+     private function show_values(){
+        $key_movement = KeyMovement::where('short','SolAlm')->first();
+        dd('warehouse_id='. $this->product_warehouse->warehouse->id,
+        'product_id='. $this->ware_request_detail->product_id,
+        'key_movement_id='. $key_movement->id,
+        'date='. now(),
+        'quantity='. $this->quantity,
+        'cost='. $this->product_warehouse->average_cost,
+        'amount='. $this->product_warehouse->average_cost * $this->quantity,
+        'reference='. $this->ware_request_detail->warehouse_request->reference,
+        'status='. 'Aplicado',
+        'user_id='. Auth::user()->id);
      }
 }
