1104044a7bbdf35370e1afa4a6fc1c98676b8c69
Federico Castañeda <ffcastaneda@gmail.com>
2025-04-15 22:27:59 -0600

    Corregir Cálculo de retencion ISR en cotizaciones

--- app/Filament/Asesor/Resources/CotizationResource.php
+++ app/Filament/Asesor/Resources/CotizationResource.php
@@ -119,8 +119,11 @@ class CotizationResource extends Resource
                                         $iva = round(($state + $envio) * 0.16, 2);
                                     }
                                     $set('iva', $iva);
-                                    $retencion_isr = round($state - $envio * env('PERCENTAGE_RETENCION_ISR',1.25)/100,2);
+                                    $percentage_retencion =  env('PERCENTAGE_RETENCION_ISR',1.25);
+                                    $base_retencion = round($state - $envio);
+                                    $retencion_isr = round($base_retencion * ( $percentage_retencion/100),2);
                                     $set('retencion_isr', $retencion_isr);
+
                                     $total = round($state + $iva - $descuento + $envio -$retencion_isr , 2);
                                     $set('total', $total);
                                 }),
@@ -151,8 +154,11 @@ class CotizationResource extends Resource
                                         $iva = round(($state + $subtotal) * 0.16, 2);
                                     }
                                     $set('iva', $iva);
-                                    $retencion_isr = round($state - $subtotal * env('PERCENTAGE_RETENCION_ISR',1.25)/100,2);
+                                    $percentage_retencion =  env('PERCENTAGE_RETENCION_ISR',1.25);
+                                    $base_retencion = round($state - $envio);
+                                    $retencion_isr = round($base_retencion * ( $percentage_retencion/100),2);
                                     $set('retencion_isr', $retencion_isr);
+
 
                                     $total = round($subtotal +  $iva - $descuento + $state, 2);
                                     $set('total', $total);
