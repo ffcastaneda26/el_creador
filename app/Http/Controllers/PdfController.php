11835b8e7cd9cd85c3d7fdc9a3f8361e8513d476
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2025-09-30 22:15:17 -0600

    Aviso de privacidad nuevo

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -70,27 +70,31 @@ class PdfController extends Controller
             $size     = $fpdi->getTemplateSize($template);
             $fpdi->AddPage($size['orientation'], [$size['width'], $size['height']]);
             $fpdi->useTemplate($template);
-            $fpdi->SetXY(134, 22);
+            $fpdi->SetXY(128, 22.5);
 
             $fpdi->Write(0, now()->format('d'));
-            $fpdi->Text(152, 23, GeneralHelp::spanish_month(now(), 's'));
+            $fpdi->Text(145, 23.2, GeneralHelp::spanish_month(now(), 'l'));
+            $fpdi->Text(170, 23.2, now()->format('Y'));
+
+
 
             if ($data) {
                 $standard_name = GeneralHelp::normalize_text($data->full_name);
                 $standard_name = ucwords($standard_name);
 
-                $fpdi->Text(42, 133, $standard_name);                                              // Nombre
-                $fpdi->Text(44, 145, $data->phone);                                                // Teléfono
-                $fpdi->Text(61, 157, strtolower($data->email));                                    // Correo
+                $fpdi->Text(50, 121, $standard_name);                                              // Nombre
+                                                              // Nombre
+                $fpdi->Text(52, 132, $data->phone);                                                // Teléfono
+                $fpdi->Text(66, 143.1, strtolower($data->email));                                    // Correo
                 $address = $data->address . ' Col: ' . $data->colony . ' en ' . $data->city->name; // Calle y número
                                                                                                    // $address = strtr($address, array_combine($buscar, $reemplazar));
                 $address = GeneralHelp::normalize_text($address);
-                $fpdi->Text(44, 170, $address);
+                $fpdi->Text(50, 155, $address);
                 $municipality_state = $data->municipality->name . ',' . $data->state->abbreviated . '   C.P. ' . $data->zipcode;
-                $fpdi->Text(44, 175, $municipality_state);
-                $fpdi->Text(90, 183, $data->rfc);     // RFC
-                $fpdi->Text(64, 195, $data->ine);     // INE
-                $fpdi->Text(40, 265, $standard_name); // Nombre para firmar
+                $fpdi->Text(50, 160, $municipality_state);
+                $fpdi->Text(92, 166.1, $data->rfc);     // RFC
+                $fpdi->Text(68, 177.5, $data->ine);     // INE
+                $fpdi->Text(30, 226, $standard_name); // Nombre para firmar
             }
         }
         return $fpdi->Output('S');
